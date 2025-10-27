854e4ed2bf01762a859a3230ed4377b5685926e0
ortigosa21 <fernando.ortigosa21@gmail.com>
2025-10-27 14:29:36 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles.css"; // se você tiver um arquivo de estilos
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
